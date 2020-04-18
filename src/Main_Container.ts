a998e3606c95c76abb7b64f61685d58d5ba6ed7b
Dmitriy <nozaki@yandex.ru>
2020-04-18 16:53:32 +0300

    init personage change window

--- src/Main_Container.ts
+++ src/Main_Container.ts
@@ -5,6 +5,7 @@ export default class Main_Container extends Container {
 	public static readonly WIDTH:number = 1500;
 	public static readonly HEIGHT:number = 900;
 	private _background:Sprite;
+	private _screenBorder:Sprite;
 
 	constructor() {
 		super();
@@ -14,8 +15,10 @@ export default class Main_Container extends Container {
 	private initImages():void {
         const loader:Loader = new Loader();
 		loader.add("background", "background.jpg");
+		loader.add("border", "border.png");
 		loader.on("complete", ()=> {
 			this.initBackground();
+			this.initPersonageChangeWindow();
 		});
 		loader.load();
 	}
@@ -23,5 +26,15 @@ export default class Main_Container extends Container {
 	private initBackground():void {
 		this._background = Sprite.from("background");
 		this.addChild(this._background);
+	}
+
+	private initPersonageChangeWindow():void {
+
+		this._screenBorder = Sprite.from("border");
+		this._screenBorder.width /= 2;
+		this._screenBorder.height /= 2;
+		this._screenBorder.x = Main_Container.WIDTH/2 - this._screenBorder.width/2;
+		this._screenBorder.y = Main_Container.HEIGHT/2 - this._screenBorder.height/2;
+		this.addChild(this._screenBorder);
 	}
 }
